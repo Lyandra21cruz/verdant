3f9febc87603a1000cde4c644f78786af8b078d6
Anna Clara <annaclaragoliveir08@gmail.com>
2025-02-12 09:54:35 -0300

    css

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>VERDANT</title>
+</head>        <img src="img/1.png" alt="Logo Verdant" class="logo">
+
+<body>
+    <div class="logo-container">
+    </div>
+    <div class="overlay"></div>
+    <div class="cabecalho">
+        <h1 class="titulo">Verdant</h1>
+        <h2 class="subtitulo">SUSTENTABILIDADE</h2>
+        <h3 class="descricao">CUIDAR DO QUE É VERDE É CUIDAR DA VIDA<br>EM SUA FORMA MAIS PURA</h3>
+    </div>
+
+    <div class="cadastro">
+        <h1>LOGIN</h1>
+        <div class="form-container">
+            <form method="POST">
+                <input type="text" name="nome_usuario" placeholder="Nome" required>
+                <br>
+                <input type="email" name="email" placeholder="Email" required>
+                <br>
+                <input type="password" name="senha" placeholder="Senha" required>
+                <button type="submit">ENTRAR</button>
+            </form>
+        </div>
+    </div>
+
+    <footer>
+        <p>&copy; 2025 Verdant. Todos os direitos reservados. Empresa Sustentável.</p>
+    </footer>
+</body>
+</html>
