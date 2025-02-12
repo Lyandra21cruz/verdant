807491f51f5b2515138bbe01864b16654e340b82
Lyandra <lyandracruz.sesisenai@gmail.com>
2025-02-12 07:31:44 -0300

    cart

--- cart/index.php
+++ cart/index.php
@@ -8,17 +8,19 @@ require('./sheep_core/config.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VERDANT</title>
-    <link rel="stylesheet" href="assests/css/style.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     
-<div class="header">
-    <p class="logo">VERDANT</p>
+<section class="section home" id="home">
+    <div class="header">
     <div class="cart">
         <p><i class="fa fa-shopping-cart"></i>0</p>
     </div>
 </div>
+    </section>
+
 
 </body>
 </html>
