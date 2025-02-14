5d83af0f831a1f1f7edd644a0c642455beebcf1a
Rafael <rafael16oliveira.sesisenai@gmail.com>
2025-02-14 10:15:23 -0300

    envio do meu codigo

---
+++ verdantp/compra/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="tituloprincipal">
+
+        <h1>VERDANT</h1>
+    </div>
+
+    <div class="container">
+
+        <h3 class="titulo">Realizar Compra</h3>
+
+    </div>
+    
+        <div class="container2">
+            <div class="cabeça">
+            <form class="principal" action="POST">
+
+                <div class="input">
+                    <input type="text" class="borda" placeholder="Seu Nome Completo">
+                </div>
+                </label>
+
+                <label for="POST">
+                    <div class="input">
+                        <input type="text" class="borda" placeholder="PIN do Produto">
+                    </div>
+                </label>
+
+                <label for="POST">
+                    <div class="input">
+                        <input type="text" class="borda" placeholder="Forma de Pagamento">
+                    </div>
+                </label>
+
+                <label for="POST">
+                    <div class="input">
+                        <input type="text" class="borda" placeholder="CEP da Casa">
+                    </div>
+                </label>
+
+
+                <a href="venda.php" class="venda-button">Vender</a>
+            </form>
+        </div>
+    </div>
+
+
+
+
+</body>
+
+</html>
