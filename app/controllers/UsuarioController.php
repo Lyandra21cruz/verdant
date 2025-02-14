6aea41a5ec93cefd2dcb7652284fed5c277a1a19
Kauã Freitas <kauapessoadefreitas@gmail.com>
2025-02-14 13:09:24 -0300

    Atualização no CRUD dos usuários

--- app/controllers/UsuarioController.php
+++ app/controllers/UsuarioController.php
@@ -2,8 +2,23 @@
 require_once "C:\Turma2\xampp\htdocs\verdant\app\models\UsuarioModel.php";
 
 class UsuarioController {
+
+    public $usuarioModel;
     public function __construct() {
         $this->usuarioModel = new UsuarioModel();
+    }
+
+    public function criarUsuario()
+    {
+        $this->usuarioModel->criarUsuario();
+    }
+    
+    public function editarUsuario($id){
+        $this->usuarioModel->editarUsuario($id);
+    }
+
+    public function deletarUsuario($id){
+        $this->usuarioModel->deletarUsuario($id);
     }
 
 
--- app/models/UsuarioModel.php
+++ app/models/UsuarioModel.php
@@ -1,6 +1,27 @@
 <?php 
 
 class UsuarioModel {
+    public $pdo;
+
+    public function criarUsuario()
+    {
+        global $pdo;
+        $stmt = $pdo->prepare("INSERT INTO usuarios () VALUES (?)");
+        $stmt->execute([]);
+    }
+    
+    public function editarUsuario($id){
+        global $pdo;
+        $stmt = $pdo->prepare("UPDATE usuarios SET  WHERE id = ?");
+        $stmt->execute([,$id]);
+    }
+
+    public function deletarUsuario($id){
+        global $pdo;
+        $stmt = $pdo->prepare("DELETE FROM usuarios WHERE id = ?");
+        $stmt->execute([$id]);
+    }
+
     
 }
 
