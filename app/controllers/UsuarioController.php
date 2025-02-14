d1ee3c340b5d5b5e8c33664b148c18cb1e22ca41
Kauã Freitas <kauapessoadefreitas@gmail.com>
2025-02-14 08:54:26 -0300

    Criação do Model e Controller para o Usuario

---
+++ app/controllers/UsuarioController.php
@@ -0,0 +1,12 @@
+<?php 
+require_once "C:\Turma2\xampp\htdocs\verdant\app\models\UsuarioModel.php";
+
+class UsuarioController {
+    public function __construct() {
+        $this->usuarioModel = new UsuarioModel();
+    }
+
+
+}
+
+?>
